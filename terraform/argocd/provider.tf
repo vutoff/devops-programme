83b8da7c8d3996cd5352a7002abe57f4e361ff8c
Dimitar Dimitrov <dhd.dimitrov@gmail.com>
2025-01-14 13:52:46 +0200

    argocd terrafrom provider is desabled for debugging

--- terraform/argocd/provider.tf
+++ terraform/argocd/provider.tf
@@ -9,14 +9,9 @@ provider "kubernetes" {
   config_context = "sof-lab03"
 }
 
-resource "kubernetes_namespace" "sof-app01" {
-  metadata {
-    name = "sof-app01"
-  }
-}
-
 terraform {
   backend "local" {
     path = "~/devops/telerik/terraform-argocd.tfstate"
   }
 }
+
