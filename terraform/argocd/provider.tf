51970c559c7e36b4b9428f1d34a232fc09649ad3
Dimitar Dimitrov <dhd.dimitrov@gmail.com>
2025-01-14 23:39:23 +0200

    separates terraform argocd application sof-app01

--- terraform/argocd/provider.tf
+++ terraform/argocd/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.10"
-  required_providers {
-    argocd = {
-      source  = "argoproj-labs/argocd"
-      version = "7.3.0"
-    }
-  }
-}
-
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/sof-lab03"
@@ -17,14 +7,6 @@ provider "helm" {
 provider "kubernetes" {
   config_path    = "~/.kube/sof-lab03"
   config_context = "sof-lab03"
-}
-
-provider "argocd" {
-  server_addr = "localhost:8080"
-  plain_text  = true
-  insecure    = true
-  username    = "admin"
-  password    = var.password
 }
 
 terraform {
---
+++ terraform/sof-app01/provider.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.10"
+  required_providers {
+    argocd = {
+      source  = "argoproj-labs/argocd"
+      version = "7.3.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/sof-lab03"
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/sof-lab03"
+  config_context = "sof-lab03"
+}
+
+provider "argocd" {
+  server_addr = "localhost:8080"
+  plain_text  = true
+  insecure    = true
+  username    = "admin"
+  password    = var.password
+}
+
+terraform {
+  backend "local" {
+    path = "/Users/dimitar.dimitrov/devops/telerik/terraform-argocd-sof-app01.tfstate"
+  }
+}
