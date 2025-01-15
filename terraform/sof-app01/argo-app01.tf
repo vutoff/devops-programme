5a7e3178294cb2889c6f50e7b60057ecb96b189a
Dimitar Dimitrov <dhd.dimitrov@gmail.com>
2025-01-15 14:09:24 +0200

    add auto sync in argocd delayment for sof-app01

--- terraform/sof-app01/argo-app01.tf
+++ terraform/sof-app01/argo-app01.tf
@@ -6,6 +6,24 @@ resource "argocd_application" "sof-app01" {
   spec {
     project = "default"
 
+    sync_policy {
+      sync_options = ["Validate=true"]
+
+      retry {
+        limit = "5"
+
+        backoff {
+          duration     = "5s"
+          factor       = "2"
+          max_duration = "3m0s"
+        }
+      }
+      automated {
+        allow_empty = false
+        prune       = false
+        self_heal   = false
+      }
+    }
     destination {
       server    = "https://kubernetes.default.svc"
       namespace = "sof-app01"
