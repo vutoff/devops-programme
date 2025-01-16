57bb7e9bcd3546159be0917df7e87526d8170140
Dimitar Dimitrov <dhd.dimitrov@gmail.com>
2025-01-16 16:07:32 +0200

    Build new Blue image with prometheus

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Green World!"
+    return "Hello, Blue World!"
 
 if __name__ == "__main__":
     app.run(port=os.environ.get("PORT", 3000), host="0.0.0.0")
