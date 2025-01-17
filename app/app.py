faf4337bf67486635cb2668803362eb0653850aa
Dimitar Dimitrov <dhd.dimitrov@gmail.com>
2025-01-17 15:26:25 +0200

    build fresh image

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Yellow World!"
+    return "Hello, Fresh World!"
 
 if __name__ == "__main__":
     app.run(port=os.environ.get("PORT", 3000), host="0.0.0.0")
